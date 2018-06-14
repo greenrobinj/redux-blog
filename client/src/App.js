8dd3e474b59a0d24350acd4925d5e9837eea2516
Robin Green <greenrj4@gmail.com>
2018-06-14 15:33:46 -0600

    init

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Blogs from './components/Blogs'
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <Blogs/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/components/Blogs.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class Blogs extends React.Component {
+
+  componentDidMount() {
+    axios.get()
+      .then( res => {
+
+      })
+        .catch
+    }
+
+
+  render() {
+    return(
+      <div>
+        <h1> BLOGS </h1>
+      </div>
+    )
+  }
+}
+
+export default Blogs;
